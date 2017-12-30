c82d77acb38abfa5810703f2df67bf295ec5586e
kaareprogram <kaareprogram@hotmail.com>
2017-12-30 18:49:50 +0100

    Oops

---
+++ AI/LinearRegressionProgram.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace AI
+{
+    class LinearRegressionProgram
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static double Value(double x1, double x2, double x3, double[] coef)
+        {
+
+        }
+
+        static double RSquared(double[][] data, double[] coef)
+        {
+
+        }
+
+        static double[][] DummyData(int rows, int seed)
+        {
+
+        }
+
+        static double[][] Design(double[][] data)
+        {
+
+        }
+
+        static double[] Solve(double[][] design)
+        {
+
+        }
+
+        static void ShowMatrix(double[][] m, int dec)
+        {
+
+        }
+
+        static void ShowVector(double[] v, int dec)
+        {
+
+        }
+
+        static double[][] MatrixTranspose(double[][] matrix)
+        {
+
+        }
+
+        static double[][] MatrixProduct(double[][] matrixA, double[][] matrixB)
+        {
+
+        }
+
+        static double[][] MatrixInverse(double[][] matrix)
+        {
+
+        }
+    }
+}
